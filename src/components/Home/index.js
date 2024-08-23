c3b6386cefe14ba26f69b0df4a9f60d24b26f681
sharonrajkondapaturi <sharoncheran@gmail.com>
2024-08-23 11:11:58 +0530

    first-commit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,9 @@ import './index.css'
 
 const Home = ()=>{
     return(
-        <h1 className='ele'>hello</h1>
+        <div className='background'>
+            <h1 className='ele'>hello</h1>
+        </div>
     )
 }
 
