af4a0ebee575398c224b2fbdb734165d30b51534
sharonrajkondapaturi <sharoncheran@gmail.com>
2024-08-23 16:57:02 +0530

    second-commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import { GiHamburgerMenu } from "react-icons/gi";
+import './index.css'
+
+const Header = ()=>{
+    const [ham,setHam] = useState(false)
+
+    const onHam = ()=>{
+        setHam(prevState=>!prevState)
+    }
+    return(
+        <header>
+            <nav className="ham" onClick={onHam}>
+                <GiHamburgerMenu fill='#fff' size={20}/>
+                {
+                ham?<ul className='mobile-ham'>
+                    <li><a href="www.google.com" className='mobile-anchors'>Home</a></li>
+                    <li><a href="www.google.com" className='mobile-anchors'>Games</a></li>
+                    <li><a href="www.google.com" className='mobile-anchors'>Consoles</a></li>
+                    <li><a href="www.google.com" className='mobile-anchors'>About</a></li>
+                </ul>:null
+                }
+            </nav>
+            <nav>
+                <ul>
+                    <li><a href="www.google.com">Home</a></li>
+                    <li><a href="www.google.com">Games</a></li>
+                    <li><a href="www.google.com">Consoles</a></li>
+                    <li><a href="www.google.com">About</a></li>
+                </ul>
+            </nav>
+            <nav>
+                <ul>
+                    <li><a href="www.google.com">UserDetails</a></li>
+                    <li><button>Logout</button></li>
+                </ul>
+            </nav>
+            <nav className='mobile-nav'>
+                <ul className='mobile-right'>
+                <li><a href="www.google.com">UserDetails</a></li>
+                <li><button className='mobile-button'>Logout</button></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,44 @@
+import Header from '../Header'
 import './index.css'
 
 const Home = ()=>{
     return(
-        <div className='background'>
-            <h1 className='ele'>hello</h1>
+        <div>
+            <Header/>
+            <section className='section-odd'>
+                <aside>
+                    <img src="https://i.blogs.es/b06eda/bo300/1366_2000.jpg" 
+                    alt="call-of-duty" 
+                    className='even-img'/>
+                </aside>
+                <article class="odd-article">
+                    <h1>Experience the war Zone</h1>
+                    <p>Call of duty Black ops 3 has Muliplayer options</p>
+                </article>
+            </section>
+            <section className='section-even'>
+                <article className='even-article'>
+                    <h1>Experience the Leap of Faith</h1>
+                    <p>After the Ezio Generation dive into new generation of Kenway</p>
+                </article>
+                <aside>
+                    <img src="https://www.godisageek.com/wp-content/uploads/assassins-creed-3-remastered-review-1024x576.jpg" 
+                    className='even-img'
+                    alt="assassins creed"/>
+                </aside>
+            </section>
+            <section className='section-odd'>
+                <aside>
+                    <img src="https://m.economictimes.com/thumb/height-450,width-600,imgsize-45140,msid-104388013/marvels-spider-man-2-set-to-release-soon-heres-all-you-need-to-know.jpg" 
+                    alt="marvel-spiderman"
+                    className='even-img'
+                    />
+                </aside>
+                <article className='odd-article'>
+                    <h1>Experience the war Zone</h1>
+                    <p>Call of duty Black ops 3 has Muliplayer options</p>
+                </article>
+            </section>
         </div>
     )
 }
