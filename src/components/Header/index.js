bbb5287ab50bed0fb1f4367678b9fe4b7b80fa96
sharonrajkondapaturi <138500511+sharonrajkondapaturi@users.noreply.github.com>
2024-08-25 15:44:18 +0530

    fourteen-commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -13,7 +13,6 @@ const Header = ()=>{
         setHam(prevState=>!prevState)
     }
 
-    //used to logout from the cookies
     const onLogout = () => {
         Cookies.remove('jwt_token')
         navigate('/login')
@@ -25,32 +24,32 @@ const Header = ()=>{
                 <GiHamburgerMenu fill='#fff' size={20} style={{position:'fixed'}}/>
                 {
                 ham?<ul className='mobile-ham'>
-                    <li style={{paddingBottom:10,borderBottomStyle:"solid",borderBottomColor:"#000"}}><a  href="https://newgameblog.onrender.com/login" className='mobile-anchors'>Home</a></li>
-                    <li style={{paddingBottom:10,borderBottomStyle:"solid",borderBottomColor:"#000"}}><a  href="https://newgameblog.onrender.com/posts" className='mobile-anchors'>Posts</a></li>
-                    <li style={{paddingBottom:10,borderBottomStyle:"solid",borderBottomColor:"#000"}}><a href="https://newgameblog.onrender.com/userposts" className='mobile-anchors'>User Posts</a></li>
+                    <li style={{paddingBottom:10,borderBottomStyle:"solid",borderBottomColor:"#000"}}><a  href="/login" className='mobile-anchors'>Home</a></li>
+                    <li style={{paddingBottom:10,borderBottomStyle:"solid",borderBottomColor:"#000"}}><a  href="/posts" className='mobile-anchors'>Posts</a></li>
+                    <li style={{paddingBottom:10,borderBottomStyle:"solid",borderBottomColor:"#000"}}><a href="/userposts" className='mobile-anchors'>User Posts</a></li>
                     <li style={{paddingBottom:5}}><a href="http://localhost:3000/newpost" className='mobile-anchors'>Add Post</a></li>
                 </ul>:null
                 }
             </nav>
             <nav>
                 <ul>
-                    <li><a href="https://newgameblog.onrender.com/">Home</a></li>
-                    <li><a href="https://newgameblog.onrender.com/posts">Posts</a></li>
-                    <li><a href="https://newgameblog.onrender.com/userposts">User Posts</a></li>
-                    <li><a href="https://newgameblog.onrender.com/newpost">Add Post</a></li>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/posts">Posts</a></li>
+                    <li><a href="/userposts">User Posts</a></li>
+                    <li><a href="/newpost">Add Post</a></li>
                 </ul>
             </nav>
             <nav>
                 <ul>
-                    <li><a href="https://newgameblog.onrender.com/register">Register</a></li>
-                    <li><a href='https://newgameblog.onrender.com/login'>Login</a></li>
+                    <li><a href="/register">Register</a></li>
+                    <li><a href='/login'>Login</a></li>
                     <li><button onClick={onLogout}>Logout</button></li>
                 </ul>
             </nav>
             <nav className='mobile-nav'>
                 <ul className='mobile-right'>
-                <li><a href="https://newgameblog.onrender.com/register">Register</a></li>
-                <li><a href="https://newgameblog.onrender.com/login">Login</a></li>
+                <li><a href="/register">Register</a></li>
+                <li><a href="/login">Login</a></li>
                 <li><button onClick={onLogout} className='mobile-button'>Logout</button></li>
                 </ul>
             </nav>
