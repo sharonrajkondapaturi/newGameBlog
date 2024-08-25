837fe2897cd603364b65471ccc5b8fd831b850e0
sharonrajkondapaturi <138500511+sharonrajkondapaturi@users.noreply.github.com>
2024-08-25 16:00:27 +0530

    fifteen-commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,7 +8,8 @@ import './index.css'
 const Header = ()=>{
     const [ham,setHam] = useState(false)
     const navigate = useNavigate()
-
+  
+    //the below functions will triger the route pages
     const onHam = ()=>{
         setHam(prevState=>!prevState)
     }
@@ -17,6 +18,28 @@ const Header = ()=>{
         Cookies.remove('jwt_token')
         navigate('/login')
     }
+
+    const onHome = () => {
+        navigate('/')
+    }
+
+    const onlogin = () => {
+        navigate('/')
+    }
+
+    const onPosts = () => {
+        navigate('/posts')
+    }
+
+    const onUserPosts = () => {
+        navigate('/userposts')
+    }
+    const onNewPost = () => {
+        navigate('/newpost')
+    }
+    const onRegister = () => {
+        navigate('/register')
+    }
     
     return(
         <header>
@@ -24,32 +47,32 @@ const Header = ()=>{
                 <GiHamburgerMenu fill='#fff' size={20} style={{position:'fixed'}}/>
                 {
                 ham?<ul className='mobile-ham'>
-                    <li style={{paddingBottom:10,borderBottomStyle:"solid",borderBottomColor:"#000"}}><a  href="/login" className='mobile-anchors'>Home</a></li>
-                    <li style={{paddingBottom:10,borderBottomStyle:"solid",borderBottomColor:"#000"}}><a  href="/posts" className='mobile-anchors'>Posts</a></li>
-                    <li style={{paddingBottom:10,borderBottomStyle:"solid",borderBottomColor:"#000"}}><a href="/userposts" className='mobile-anchors'>User Posts</a></li>
-                    <li style={{paddingBottom:5}}><a href="http://localhost:3000/newpost" className='mobile-anchors'>Add Post</a></li>
+                    <li style={{paddingBottom:10,borderBottomStyle:"solid",borderBottomColor:"#000"}}><a  onClick={onHome} className='mobile-anchors'>Home</a></li>
+                    <li style={{paddingBottom:10,borderBottomStyle:"solid",borderBottomColor:"#000"}}><a  onClick={onPosts} className='mobile-anchors'>Posts</a></li>
+                    <li style={{paddingBottom:10,borderBottomStyle:"solid",borderBottomColor:"#000"}}><a onClick={onUserPosts} className='mobile-anchors'>User Posts</a></li>
+                    <li style={{paddingBottom:5}}><a onClick={onNewPost} className='mobile-anchors'>Add Post</a></li>
                 </ul>:null
                 }
             </nav>
             <nav>
                 <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/posts">Posts</a></li>
-                    <li><a href="/userposts">User Posts</a></li>
-                    <li><a href="/newpost">Add Post</a></li>
+                    <li><a onClick={onHome}>Home</a></li>
+                    <li><a onClick={onPosts}>Posts</a></li>
+                    <li><a onClick={onUserPosts}>User Posts</a></li>
+                    <li><a onClick={onNewPost}>Add Post</a></li>
                 </ul>
             </nav>
             <nav>
                 <ul>
-                    <li><a href="/register">Register</a></li>
-                    <li><a href='/login'>Login</a></li>
+                    <li><a onClick={onRegister}>Register</a></li>
+                    <li><a onClick={onlogin}>Login</a></li>
                     <li><button onClick={onLogout}>Logout</button></li>
                 </ul>
             </nav>
             <nav className='mobile-nav'>
                 <ul className='mobile-right'>
-                <li><a href="/register">Register</a></li>
-                <li><a href="/login">Login</a></li>
+                <li><a onClick={onRegister}>Register</a></li>
+                <li><a onClick={onlogin}>Login</a></li>
                 <li><button onClick={onLogout} className='mobile-button'>Logout</button></li>
                 </ul>
             </nav>
